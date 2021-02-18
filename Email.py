7c41a1c9ef694a3b590373da84fd9c47217f4cc9
replay1121 <replay10540@gmail.com>
2021-02-18 19:47:57 +0700

    55

--- Email.py
+++ Email.py
@@ -1,7 +1,7 @@
 import json
 import smtplib
 import os
-
+from os.path import basename
 from string import Template
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -10,35 +10,143 @@ from datetime import datetime
 email_address = 'replay10540@gmail.com'
 email_password = '0875507608' 
 
-data = {"my": "foo", "yours": "bar"}
+
 # send_email(data)
-def sentMail():
+
+
+def sentMail(message):
     try:
         sending_ts = datetime.now()
-        sub = "หัวข้อไง %s" % sending_ts.strftime('%Y-%m-%d %H:%M:%S')
+        sub = "New Smartphones Siamphone %s" % sending_ts.strftime('%Y-%m-%d %H:%M:%S')
         msg = MIMEMultipart('alternative')
         msg['From'] = email_address
         msg['To'] = email_address
         msg['Subject'] = sub
+        
+        data = ""
+        for item in message:
+            data = data + format("<tr><td>"+item["brandname"]+"</td><td>"+item["name"]+"</td><td>"+item["price"]+"</td>"
+            "<td>"+item["newprice"]+"</td>""<td>"+item["datetime"]+"</td>""<td><a href="+item["link"]+">ดูข้อมูล</a></td></tr>")
+        html = """\
+        <html>
+          <head>
+          <style>body{
+            font:1.2em normal Arial,sans-serif;
+            color:#34495E;
+            }
 
-        body = "ส่งได้แล้วววว"
-        msg.attach(MIMEText(body, 'plain'))
+            h1{
+            text-align:center;
+            text-transform:uppercase;
+            letter-spacing:-2px;
+            font-size:2.5em;
+            margin:20px 0;
+            }
 
-        attachment = MIMEText(json.dumps(data))
-        attachment.add_header('Content-Disposition', 'attachment', 
-                            filename="data_file.json")
-        msg.attach(attachment)
+            .container{
+            width:90%;
+            margin:auto;
+            }
+
+            table{
+            border-collapse:collapse;
+            width:100%;
+            }
+
+            .blue{
+            border:2px solid #1ABC9C;
+            }
+
+            .blue thead{
+            background:#1ABC9C;
+            }
+
+            .purple{
+            border:2px solid #9B59B6;
+            }
+
+            .purple thead{
+            background:#9B59B6;
+            }
+
+            thead{
+            color:white;
+            }
+
+            th,td{
+            text-align:center;
+            padding:5px 0;
+            }
+
+            tbody tr:nth-child(even){
+            background:#ECF0F1;
+            }
+
+            tbody tr:hover{
+            background:#BDC3C7;
+            color:#FFFFFF;
+            }
+
+            .fixed{
+            top:0;
+            position:fixed;
+            width:auto;
+            display:none;
+            border:none;
+            }
+
+            .scrollMore{
+            margin-top:600px;
+            }
+
+            .up{
+            cursor:pointer;
+            }</style>
+          </head>
+          <body>
+           <h1>&darr; NEWITEM &darr;</h1>
+            <table class="blue">
+            <thead>
+                <tr>
+                <th>Brand</th>
+                <th>NamePhone</th>
+                <th>price</th>
+                <th>newprice</th>
+                <th>date</th>
+                <th>image</th>
+                </tr>
+            </thead>
+            <tbody>
+               """+data+"""
+            </tbody>
+            </table>
+          </body>
+        </html>
+        """
+        print(html)
+        #msg.attach(MIMEText(body,'plain'))
+        msg.attach(MIMEText(html, 'html'))
+
+        
+
+        # attachment = MIMEText(json.dumps(data))
+        # attachment.add_header('Content-Disposition', 'attachment', 
+        #                     filename="data_file.json")
+        # msg.attach(attachment)
+        
 
         # print(msg)
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        server.ehlo()
-        server.login(email_address, email_password)
-        # server.sendmail(email_address, email_address, "msg")
-        server.send_message(msg)
-        server.close()
+        # server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        # server.ehlo()
+        # server.login(email_address, email_password)
+        # # server.sendmail(email_address, email_address, "msg")
+        # server.send_message(msg)
+        # server.close()
 
         print ('Email sent!')
-    except:
+    except ValueError:
         print ('Something went wrong...')
+        print (ValueError)
 
-sentMail()
+
+
--- Scraping.py
+++ Scraping.py
@@ -3,43 +3,84 @@ import urllib, json
 from bs4 import BeautifulSoup
 import datetime
 from ast import literal_eval
-url = "https://store.siamphone.com/services/spec/test?&order_by=release_date%20desc"
-
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Max-Age': '3600',
-    'Accept-Encoding': 'identity',
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content,'html.parser')
-datanow = datetime.datetime.now().strftime("%Y-%m")
+from Email import sentMail
+import time
+import timeit
 
 
-response =  json.loads(str(soup))
-data = response['hits']['hits']
-sucessdata = [];
-for i in range(len(data)):
-  datephone = datetime.datetime.strptime(data[i]['_source']['release_date_t'], '%Y-%m-%d').strftime("%Y-%m")
-  if datanow == datephone :
-      if data[i]['_source']['price'] != 0 :
-        newdata = {
-            "brandname":data[i]['_source']['brand_name'],
-            "name":data[i]['_source']['model_fullname'],
-            "price":data[i]['_source']['price'],
-            "newprice":data[i]['_source']['new_price'],
-            "datetime":data[i]['_source']['release_date_t'],
-            "image":data[i]['_source']['image_pic1'],
+
+def scarping():
+    url = "https://store.siamphone.com/services/spec/test?&order_by=release_date%20desc"
+
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'Accept-Encoding': 'identity',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
         }
-        # print(newdata)
-        sucessdata.append(newdata)
-# print(sucessdata);
-# with open("data_file.json", "w") as write_file:
-#     json.dump(sucessdata, write_file)
-fileold = literal_eval(open("data_file.json", "r").read())
-for item in fileold:
+    req = requests.get(url, headers)
+    soup = BeautifulSoup(req.content,'html.parser')
+    datanow = datetime.datetime.now().strftime("%Y-%m")
+
+
+    response =  json.loads(str(soup))
+    data = response['hits']['hits']
+    sucessdata = []
+    for i in range(len(data)):
+        datephone = datetime.datetime.strptime(data[i]['_source']['release_date_t'], '%Y-%m-%d').strftime("%Y-%m")
+        if datanow == datephone :
+         if data[i]['_source']['price'] != 0 :
+            newdata = {
+                "brandname":data[i]['_source']['brand_name'],
+                "name":data[i]['_source']['model_fullname'],
+                "price":str(data[i]['_source']['price']),
+                "newprice":str(data[i]['_source']['new_price']),
+                "datetime":data[i]['_source']['release_date_t'],
+                # "image":data[i]['_source']['image_pic1'],
+                "link":data[i]['_source']['link_spec']
+            }
+            # print(newdata)
+            sucessdata.append(newdata)
+    # print(sucessdata);
+    # with open("data_file.json", "w") as write_file:
+    #      json.dump(sucessdata, write_file)
+    fileold = literal_eval(open("data_file.json", "r").read())
+    
+    datanewprice = []
     for checkitem in sucessdata:
-           if item['price'] != checkitem['price']:
-               print(checkitem)
+        for item in fileold:
+            if  checkitem['name']  == item['name']:
+                if  checkitem['price'] != item['price'] :
+                    # print(checkitem)
+                    datanewprice.append(checkitem)
+                elif item['name'] != checkitem['name']:
+                    #   print("newitem",checkitem)
+                    datanewprice.append(checkitem)
+                else:
+                    print("newitem",checkitem)
+
+    #print(checkitems)
+    #sentMail(checkitems)
+    #success()
+    
+               
+def countdown(t): 
+    while t: 
+            mins, secs = divmod(t, 60) 
+            timer = '{:02d}:{:02d}'.format(mins, secs) 
+            print(timer, end="\r") 
+            time.sleep(1)
+            t -= 1
+    success()
+
+    
+def success():
+    countdown(int(10))
+    scarping()
+
+scarping()
+#countdown()
+#countdown(int(5)) 
+#success()
